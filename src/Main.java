07c46068ec7a8c5f811d64b9a0cf0568c044e32b
michael <michael@busnet.it>
2023-05-21 10:08:59 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        AutoPool<Integer> pool = new AutoPool<>();
+        Random rnd = new Random();
+        for (int i = 0; i < 5; ++i) {
+            pool.add(i);
+        }
+
+        try {
+            while (true) {
+                Resource<Integer> r = pool.acquire();
+                System.out.println("using " + r.get());
+                Thread.sleep(Math.abs(rnd.nextInt() % 1000));
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
